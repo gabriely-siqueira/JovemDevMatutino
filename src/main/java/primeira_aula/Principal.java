0969502a782675ee895581a1cb7c74523326af7e
unknown <gabrielysiqueira2004@gmail.com>
2023-05-22 11:39:26 -0300

    Alteração main

--- src/main/java/primeira_aula/Principal.java
+++ src/main/java/primeira_aula/Principal.java
@@ -10,9 +10,9 @@ public class Principal extends Pessoa {
 		pessoa1.sexo = JOptionPane.showInputDialog("Digite o sexo:");
 		pessoa1.peso = Double.parseDouble(JOptionPane.showInputDialog("Digite o peso:"));
 		pessoa1.altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura:"));
-		double resultadoIMC = calculaIMC(peso, altura);
+		double resultadoIMC2 = calculaIMC(IMC, sexo);
 		System.out.println("Nome:" + pessoa1.nome + "\nSexo: " + pessoa1.sexo + "\nPeso: " + pessoa1.peso + "\nAltura: "
-				+ pessoa1.altura + "\nIMC: " + resultadoIMC);
+				+ pessoa1.altura + "\nIMC: " + resultadoIMC2);
 
 	}
 
@@ -22,4 +22,36 @@ public class Principal extends Pessoa {
 		return IMC;
 	}
 
+	public static String imcSexo(double IMC, String sexo) {
+		if (sexo == "feminino") {
+			if (IMC < 19.1) {
+				return "Abaixo do normal";
+			} else if (IMC >= 19.1 && IMC <= 25.8) {
+				return "Peso ideal";
+			} else if (IMC >= 25.9 && IMC <= 27.3) {
+				return "Pouco acima do peso";
+			} else if (IMC >= 27.4 && IMC <= 32.3) {
+				return "Acima do peso";
+			} else {
+				return "Obesidade";
+			}
+		} else {
+			if (sexo == "masculino") {
+				if (IMC < 20.7) {
+					return "Abaixo do normal";
+				} else if (IMC >= 20.7 && IMC <= 26.4) {
+					return "Peso ideal";
+				} else if (IMC >= 26.5 && IMC <= 27.8) {
+					return "Pouco acima do peso";
+				} else if (IMC >= 27.9 && IMC <= 31.1) {
+					return "Acima do peso";
+				} else {
+					return "Obesidade";
+				}
+
+			}
+
+		}
+
+	}
 }
